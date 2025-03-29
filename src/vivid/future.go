06131505f19ed7af3363b7b7ae8106ea7f01a9d9
kercylan <kercylan@gmail.com>
2025-03-29 17:06:16 +0800

    feat(future): 为 Future 增加 Wait 函数，用于仅等待结果的场景

--- src/vivid/future.go
+++ src/vivid/future.go
@@ -3,34 +3,41 @@ package vivid
 import "fmt"
 
 func TypedFutureFrom[M Message](f Future) TypedFuture[M] {
-	return &typedFuture[M]{
-		f: f,
-	}
+    return &typedFuture[M]{
+        f: f,
+    }
 }
 
 type TypedFuture[M Message] interface {
-	Result() (m M, err error)
+    Result() (m M, err error)
 
-	Close(err error)
+    Wait() (err error)
+
+    Close(err error)
 }
 
 type typedFuture[M Message] struct {
-	f Future
+    f Future
 }
 
 func (t *typedFuture[M]) Result() (m M, err error) {
-	var result any
-	result, err = t.f.Result()
-	if err != nil {
-		return m, err
-	}
-	m, ok := result.(M)
-	if !ok {
-		return m, fmt.Errorf("future result is not of type %T, got %T", m, result)
-	}
-	return m, nil
+    var result any
+    result, err = t.f.Result()
+    if err != nil {
+        return m, err
+    }
+    m, ok := result.(M)
+    if !ok {
+        return m, fmt.Errorf("future result is not of type %T, got %T", m, result)
+    }
+    return m, nil
+}
+
+func (t *typedFuture[M]) Wait() (err error) {
+    _, err = t.Result()
+    return err
 }
 
 func (t *typedFuture[M]) Close(err error) {
-	t.f.Close(err)
+    t.f.Close(err)
 }
