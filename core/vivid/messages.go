1d53e90ec8463267787f703983d43741eafb5d3e
kercylan <kercylan@gmail.com>
2025-06-21 00:58:59 +0800

    feat(actor): 新增 Actor 生命周期监视功能

--- core/vivid/messages.go
+++ core/vivid/messages.go
@@ -1,26 +1,29 @@
 package vivid
 
 type (
-    // Message 定义了 Actor 系统中消息的通用类型。
-    //
-    // 在 vivid 框架中，任何类型的数据都可以作为消息在 Actor 之间传递。
-    Message           = any
-    internalMessageId = uint16
-    internalMessage   interface {
-        marshal() (internalMessageId, []byte)
-        unmarshal(b []byte)
-    }
+	// Message 定义了 Actor 系统中消息的通用类型。
+	//
+	// 在 vivid 框架中，任何类型的数据都可以作为消息在 Actor 之间传递。
+	Message           = any
+	internalMessageId = uint16
+	internalMessage   interface {
+		marshal() (internalMessageId, []byte)
+		unmarshal(b []byte)
+	}
 )
 
 const (
-    onLaunchMessageId internalMessageId = iota + 1
-    onKillMessageId
-    onPreRestartMessageId
-    onRestartMessageId
+	onLaunchMessageId internalMessageId = iota + 1
+	onKillMessageId
+	onPreRestartMessageId
+	onRestartMessageId
+	onWatchMessageId
+	onUnWatchMessageId
+	onWatchEndMessageId
 )
 
 var (
-    onLaunchInstance = &OnLaunch{}
+	onLaunchInstance = &OnLaunch{}
 )
 
 // OnLaunch 表示 Actor 启动消息。
@@ -30,11 +33,11 @@ var (
 type OnLaunch struct{}
 
 func (m *OnLaunch) marshal() (internalMessageId, []byte, error) {
-    return onLaunchMessageId, nil, nil
+	return onLaunchMessageId, nil, nil
 }
 
 func (m *OnLaunch) unmarshal(b []byte) error {
-    return nil
+	return nil
 }
 
 // OnKill 表示 Actor 终止消息。
@@ -50,42 +53,42 @@ func (m *OnLaunch) unmarshal(b []byte) error {
 //
 // 在此消息中如果发生致命错误，将会跳过监管策略来确保生命周期的正常结束。
 type OnKill struct {
-    operator ActorRef // 操作者
-    reason   []string // 停止原因
-    poison   bool     // 是否为优雅停止
-    applied  bool     // 是否已应用，避免将自身的停止信号传递给其他 Actor 导致错误的行为
+	operator ActorRef // 操作者
+	reason   []string // 停止原因
+	poison   bool     // 是否为优雅停止
+	applied  bool     // 是否已应用，避免将自身的停止信号传递给其他 Actor 导致错误的行为
 }
 
 func newOnKill(operator ActorRef, poison bool, reason []string) *OnKill {
-    return &OnKill{
-        operator: operator,
-        poison:   poison,
-        reason:   reason,
-    }
+	return &OnKill{
+		operator: operator,
+		poison:   poison,
+		reason:   reason,
+	}
 }
 
 func (m *OnKill) marshal() (internalMessageId, []byte) {
-    return onKillMessageId, newWriterCapacity(32).
-        writeBool(m.poison).
-        writeBool(m.applied).
-        writeStrings(m.reason).
-        writeString(m.operator.GetAddress()).
-        writeString(m.operator.GetPath()).
-        bytes()
+	return onKillMessageId, newWriterCapacity(32).
+		writeBool(m.poison).
+		writeBool(m.applied).
+		writeStrings(m.reason).
+		writeString(m.operator.GetAddress()).
+		writeString(m.operator.GetPath()).
+		bytes()
 }
 
 func (m *OnKill) unmarshal(b []byte) {
-    var address string
-    var path string
-    newReader(b).
-        readBoolTo(&m.poison).
-        readBoolTo(&m.applied).
-        readStringsTo(&m.reason).
-        readWith(func(r *reader) {
-            r.readStringTo(&address)
-            r.readStringTo(&path)
-            m.operator = NewActorRef(address, path)
-        })
+	var address string
+	var path string
+	newReader(b).
+		readBoolTo(&m.poison).
+		readBoolTo(&m.applied).
+		readStringsTo(&m.reason).
+		readWith(func(r *reader) {
+			r.readStringTo(&address)
+			r.readStringTo(&path)
+			m.operator = NewActorRef(address, path)
+		})
 }
 
 // IsPoison 返回是否为优雅停止。
@@ -94,12 +97,12 @@ func (m *OnKill) unmarshal(b []byte) {
 //   - true: 优雅停止，Actor 会等待当前消息处理完成后停止
 //   - false: 强制停止，Actor 会立即停止
 func (m *OnKill) IsPoison() bool {
-    return m.poison
+	return m.poison
 }
 
 // Reason 返回停止的原因说明。
 func (m *OnKill) Reason() []string {
-    return m.reason
+	return m.reason
 }
 
 // OnKilled 表示 Actor 已终止的通知消息。
@@ -113,63 +116,63 @@ func (m *OnKill) Reason() []string {
 //   - reason: 停止的原因说明
 //   - poison: 是否为优雅停止
 type OnKilled struct {
-    operator ActorRef // 操作者
-    ref      ActorRef // 被停止的 Actor
-    reason   []string // 停止原因
-    poison   bool     // 是否为优雅停止
+	operator ActorRef // 操作者
+	ref      ActorRef // 被停止的 Actor
+	reason   []string // 停止原因
+	poison   bool     // 是否为优雅停止
 }
 
 func newOnKilled(operator, ref ActorRef, poison bool, reason []string) *OnKilled {
-    return &OnKilled{
-        operator: operator,
-        ref:      ref,
-        poison:   poison,
-        reason:   reason,
-    }
+	return &OnKilled{
+		operator: operator,
+		ref:      ref,
+		poison:   poison,
+		reason:   reason,
+	}
 }
 
 func (m *OnKilled) marshal() (internalMessageId, []byte) {
-    return onKillMessageId, newWriterCapacity(32).
-        writeBool(m.poison).
-        writeStrings(m.reason).
-        writeString(m.ref.GetAddress()).
-        writeString(m.ref.GetPath()).
-        writeString(m.operator.GetAddress()).
-        writeString(m.operator.GetPath()).
-        bytes()
+	return onKillMessageId, newWriterCapacity(32).
+		writeBool(m.poison).
+		writeStrings(m.reason).
+		writeString(m.ref.GetAddress()).
+		writeString(m.ref.GetPath()).
+		writeString(m.operator.GetAddress()).
+		writeString(m.operator.GetPath()).
+		bytes()
 }
 
 func (m *OnKilled) unmarshal(b []byte) {
-    var address string
-    var path string
-    newReader(b).
-        readBoolTo(&m.poison).
-        readStringsTo(&m.reason).
-        readWith(func(r *reader) {
-            r.readStringTo(&address)
-            r.readStringTo(&path)
-            m.ref = NewActorRef(address, path)
-        }).
-        readWith(func(r *reader) {
-            r.readStringTo(&address)
-            r.readStringTo(&path)
-            m.operator = NewActorRef(address, path)
-        })
+	var address string
+	var path string
+	newReader(b).
+		readBoolTo(&m.poison).
+		readStringsTo(&m.reason).
+		readWith(func(r *reader) {
+			r.readStringTo(&address)
+			r.readStringTo(&path)
+			m.ref = NewActorRef(address, path)
+		}).
+		readWith(func(r *reader) {
+			r.readStringTo(&address)
+			r.readStringTo(&path)
+			m.operator = NewActorRef(address, path)
+		})
 }
 
 // Ref 返回已停止的 Actor 引用。
 func (m *OnKilled) Ref() ActorRef {
-    return m.ref
+	return m.ref
 }
 
 // IsPoison 返回是否为优雅停止。
 func (m *OnKilled) IsPoison() bool {
-    return m.poison
+	return m.poison
 }
 
 // Reason 返回停止的原因说明。
 func (m *OnKilled) Reason() []string {
-    return m.reason
+	return m.reason
 }
 
 // OnPreRestart 表示 Actor 重启前的准备消息。
@@ -182,10 +185,11 @@ type OnPreRestart struct {
 }
 
 func (m *OnPreRestart) marshal() (internalMessageId, []byte) {
-    return onPreRestartMessageId, nil
+	return onPreRestartMessageId, nil
 }
 
 func (m *OnPreRestart) unmarshal(b []byte) {
+	return
 }
 
 // OnRestart 表示 Actor 重启消息。
@@ -198,8 +202,63 @@ type OnRestart struct {
 }
 
 func (m *OnRestart) marshal() (internalMessageId, []byte) {
-    return onRestartMessageId, nil
+	return onRestartMessageId, nil
 }
 
 func (m *OnRestart) unmarshal(b []byte) {
+	return
+}
+
+// onWatch 表示 Actor 开始被监视的消息。
+//
+// 当一个 Actor 开始被监视时，会收到此消息。
+// 这标志着 Actor 的生命周期开始被监视，在生命周期结束时，将会告知监视者。
+type onWatch struct{}
+
+func (m *onWatch) marshal() (internalMessageId, []byte) {
+	return onWatchMessageId, nil
+}
+
+func (m *onWatch) unmarshal(b []byte) {
+	return
+}
+
+// onUnWatch 表示 Actor 停止被监视的消息。
+type onUnWatch struct{}
+
+func (m *onUnWatch) marshal() (internalMessageId, []byte) {
+	return onUnWatchMessageId, nil
+}
+
+func (m *onUnWatch) unmarshal(b []byte) {
+	return
+}
+
+// OnWatchEnd 表示 Actor 生命周期停止的消息。
+//
+// 当一个 Actor 生命周期停止时，会向其监视者发送此消息。
+// 这标志着 Actor 生命周期已经结束，监视者可以进行相应的清理工作。
+type OnWatchEnd struct {
+	ref    ActorRef // 被监视的 Actor
+	reason []string // 停止原因
+}
+
+func (m *OnWatchEnd) marshal() (internalMessageId, []byte) {
+	return onWatchEndMessageId, newWriterCapacity(32).
+		writeStrings(m.reason).
+		writeString(m.ref.GetAddress()).
+		writeString(m.ref.GetPath()).
+		bytes()
+}
+
+func (m *OnWatchEnd) unmarshal(b []byte) {
+	var address string
+	var path string
+	newReader(b).
+		readStringsTo(&m.reason).
+		readWith(func(r *reader) {
+			r.readStringTo(&address)
+			r.readStringTo(&path)
+			m.ref = NewActorRef(address, path)
+		})
 }
