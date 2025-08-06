590acc54b2092909822b5eecc8a2159048cbfb61
kercylan <kercylan@gmail.com>
2025-08-06 14:59:38 +0800

    docs(router): 优化路由器功能部分注释

--- pkg/vivid/router_selector.go
+++ pkg/vivid/router_selector.go
@@ -13,10 +13,13 @@ var (
 	})
 )
 
+// RouterSelector 是用于从路由表中选择需要分发的 Actor 的选择器。
 type RouterSelector interface {
+	// Balance 从 tab 中选择处理消息的 Actor
 	Balance(tab RoutingTable) []ActorRef
 }
 
+// RouterSelectorFN 是 RouterSelector 的函数式类型
 type RouterSelectorFN func(tab RoutingTable) []ActorRef
 
 func (fn RouterSelectorFN) Balance(tab RoutingTable) []ActorRef {
@@ -34,6 +37,8 @@ func NewRandomRouterSelector() RouterSelector {
 }
 
 // NewRoundRobinRouterSelector 此策略以循环方式向其路由发送消息。对于通过路由器发送的 n 条消息，每个 Actor 都会转发一条消息。
+//
+// 该选择器是有状态的，应避免在多个 Actor 之间共享。
 func NewRoundRobinRouterSelector() RouterSelector {
 	var idx = 0
 	return RouterSelectorFN(func(tab RoutingTable) []ActorRef {
